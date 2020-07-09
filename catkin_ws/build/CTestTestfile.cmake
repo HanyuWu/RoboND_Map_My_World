607f6dd30a8418bfce2d9b695d029c319ceb0dd5
HanyuWu <wuh199410@gmail.com>
2020-07-09 03:12:57 -0400

    [Hanyu] (#00) Initial commit, Project Finish

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/han/mygit/Map_My_World/catkin_ws/src
+# Build directory: /home/han/mygit/Map_My_World/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("teleop_twist_keyboard")
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/han/mygit/Map_My_World/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/han/mygit/Map_My_World/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/han/mygit/Map_My_World/catkin_ws/devel;/opt/ros/melodic' != '' else []
